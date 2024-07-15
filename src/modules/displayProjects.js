ab43582db29cefe286f8f714551c1e6fd295630e
Koulie <kouliekyriakos@hotmail.com>
2024-07-15 13:51:26 -0400

    Updated project table so that it displays the todos with more info

--- src/modules/displayProjects.js
+++ src/modules/displayProjects.js
@@ -8,27 +8,56 @@ export default function (parent, list) {
         table.classList.add('project-table');
         let thead = document.createElement('thead');
         let headRowOne = document.createElement('tr');
+        headRowOne.rowSpan = 2;
         let colOne = document.createElement('th');
+        colOne.colSpan = 4;
         colOne.textContent = project.name;
         let headRowTwo = document.createElement('tr');
-
+        headRowTwo.rowSpan = 2;
         let colTwo = document.createElement('th');
+        colTwo.colSpan = 4;
+
         colTwo.textContent = project.description;
+        let headRowThree = document.createElement('tr');
+
+        let titleCol = document.createElement('th');
+        titleCol.textContent = 'Title';
+        headRowThree.appendChild(titleCol);
+
+        let dueDateCol = document.createElement('th');
+        dueDateCol.textContent = 'Due-Date';
+        headRowThree.appendChild(dueDateCol);
+
+        let priorityCol = document.createElement('th');
+        priorityCol.textContent = 'Priority';
+        headRowThree.appendChild(priorityCol);
+
+
+
         
         headRowOne.appendChild(colOne);
-        headRowTwo.appendChild(colTwo);
+        headRowTwo.appendChild(colTwo);        
         thead.appendChild(headRowOne);
         thead.appendChild(headRowTwo);
+        thead.appendChild(headRowThree);
         table.appendChild(thead);
         parent.appendChild(table);
 
         let tableBody = document.createElement('tbody');
         if (project.associatedTodos.length > 0){
+            
             project.associatedTodos.forEach((singleTodo) => {
                 let row = document.createElement('tr');
                 let nameColumn = document.createElement('td');
                 nameColumn.textContent = singleTodo.title;
                 row.appendChild(nameColumn);
+                let dueDateColumn = document.createElement('td');
+                dueDateColumn.textContent = singleTodo.dueDate;
+                row.appendChild(dueDateColumn);
+                let priorityColumn = document.createElement('td');
+                priorityColumn.textContent = singleTodo.priority;
+                row.appendChild(priorityColumn);
+
                 tableBody.appendChild(row);
             })
         }
